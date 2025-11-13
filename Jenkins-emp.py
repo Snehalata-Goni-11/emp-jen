e92ad2506fc81c3abf4b294850118f884c24690c
Snehalata-Goni-11 <snehagoni60@gmail.com>
2025-11-13 16:58:01 +0530

    Create Jenkins-emp.py

---
+++ Jenkins-emp.py
@@ -0,0 +1,11 @@
+emp_name=snehalata
+emp_age=26
+emp_salary=25000
+emp_id=1001
+emp_add=hubli
+
+print("Employee Name:",emp_name)
+print("Employee Age:",emp_age)
+print("Employee Salary:",emp_salary)
+print("Employee ID:",emp_id)
+print("Employee Address:",emp_add)
